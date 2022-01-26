bfc399ba18e3de7e88a7e7087638817888acabee
bst003 <bstanner33@yahoo.com>
2022-01-25 23:41:16 -0500

    Update to include new helper function

--- scripts.js
+++ scripts.js
@@ -9,13 +9,22 @@ let compScore = 0;
 
 for (let i = 0; i <+ 5; i++ ) {
 
-    let playerSelectionPrompt = prompt('Choose rock, paper or scissors');
-    let playerSelection =  ( playerSelectionPrompt === null )  ? 'paper' : playerSelectionPrompt.toLowerCase();
-
+    const playerSelection = playerPrompt();
     const computerSelection = computerPlay();
 
     console.log(playRound(playerSelection, computerSelection));
     console.log( `Player score: ${playerScore}, Computer Score: ${compScore}` );
+
+}
+
+function playerPrompt() {
+
+    let playerSelectionPrompt = prompt('Choose rock, paper or scissors');
+    
+    // Prevent null inputs in case user hits escape
+    let playerFinalSelection = ( playerSelectionPrompt === null )  ? 'paper' : playerSelectionPrompt.toLowerCase();
+
+    return playerFinalSelection;
 
 }
 
