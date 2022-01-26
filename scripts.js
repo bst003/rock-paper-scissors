d7a67ae2ad933adfd81fe3d057c37644d64d261d
bst003 <bstanner33@yahoo.com>
2022-01-25 21:19:06 -0500

    Set up file structure, write psuedocode

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// Gather the player's choice from a prompt onto a variable
+// Create a function to randomize the 3 computer options
+// Use the players choice and the computerPlay function in a playRound functions
+// Compare the choices to determine the winner
+// Create a string that states both chocies and lets the user know if they won or lost.
+
+const playerSelection = prompt('Choose rock, paper or scissors');
+const computerSelection = computerPlay();
+
+function computerPlay() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+
+console.log(playRound(playerSelection, computerSelection));
