b88661313e008c23e391811accb4ce85e9fc20c2
bst003 <bstanner33@yahoo.com>
2022-02-09 22:44:31 -0500

    Add helper functions to simplify playRound

--- scripts.js
+++ scripts.js
@@ -17,6 +17,128 @@ buttons.forEach((button) => {
     button.addEventListener('click', playRound)
 });
 
+/////////////////
+// HELPER FUNCTIONS
+/////////////////
+
+// Randomly return choice for computer
+function computerPlay() {
+
+    // Create array of computer choices
+    let compChoices = new Array('rock', 'paper', 'scissors');
+
+    // get a random number to choose array item
+    let randomNumber = Math.floor( Math.random() * compChoices.length );
+
+    // store random array item in variable
+    let compChoice = compChoices[randomNumber];
+
+    //return random array item
+    return compChoice;
+
+}
+
+// Take player selection and computer selection to determine round winner
+function determineRoundResult(playerSelection, computerSelection) {
+
+    let result = 'lose';
+
+    if ( playerSelection === 'paper' && computerSelection === 'rock'){
+
+        result = 'win';
+
+    } else if ( playerSelection === 'rock' && computerSelection === 'scissors'){
+
+        result = 'win';
+
+    } else if ( playerSelection === 'scissors' && computerSelection === 'paper' ){
+
+        result = 'win';
+
+    } else if ( playerSelection === computerSelection ){
+
+        result = 'tie';
+
+    }
+
+    return result;
+
+}
+
+function updateScoreBoard( roundResult ) {
+
+    if (roundResult === 'win' ){
+        playerScore++;
+        playerScoreboard.innerText = `Player: ${playerScore}`;
+    } else if( roundResult === 'lose' ) {
+        compScore++;
+        computerScoreboard.innerText = `Computer: ${compScore}`;
+    }
+
+}
+
+function endGameDeclareWinner( playerScore, compScore, scoreGoal ) {
+
+    let gameWinner;
+    let gameResultMessage;
+
+    if( compScore >= scoreGoal || playerScore >= scoreGoal ){
+
+        gameWinner = 'Player';
+        if( compScore === scoreGoal ) {
+            gameWinner = 'Computer';
+        }
+
+        // Set gameResultMessage
+        gameResultMessage = `The score limit has been reached, ${gameWinner} is the winner`;
+
+        gameResults.innerText = gameResultMessage;
+
+        resetButton.style.display = "block";
+
+        // Remove event listener as score goal has been reached
+        buttons.forEach((button) => {
+            button.removeEventListener('click', playRound);
+            button.classList.add('disabled');
+        });
+
+    }
+
+}
+
+/////////////////
+// MAIN FUNCTIONS
+/////////////////
+
+function playRound(e) {
+
+    // Set game score goal
+    let scoreGoal = 5;
+
+    // Get computer and player selections
+    let computerSelection = computerPlay();
+    let playerSelection = e.currentTarget.getAttribute('data-value');
+
+    // Determine the round result
+    let roundResult = determineRoundResult(playerSelection, computerSelection);
+
+    // Prep message variable for later use
+    let roundResultMessage;
+
+    // Increase the score variables and update the scoreboard
+    updateScoreBoard(roundResult);
+
+    // Update the round results message
+    roundResultMessage = `You chose ${playerSelection}, computer chose ${computerSelection}, you ${roundResult}!`;
+    roundResults.innerText = roundResultMessage;
+
+    // Determine if game is over, and if so who is winner
+    endGameDeclareWinner( playerScore, compScore, scoreGoal )
+
+}
+
+
+// Reset the game back to original state
 function resetGame() {
 
     // Set scores back to 0
@@ -40,95 +162,5 @@ function resetGame() {
 
     // Hide reset buttons
     resetButton.style.display = "none";
-
-}
-
-// Randomly return choice for computer
-function computerPlay() {
-
-    // Create array of computer choices
-    let compChoices = new Array('rock', 'paper', 'scissors');
-
-    // get a random number to choose array item
-    let randomNumber = Math.floor( Math.random() * compChoices.length );
-
-    // store random array item in variable
-    let compChoice = compChoices[randomNumber];
-
-    //return random array item
-    return compChoice;
-
-}
-
-
-function playRound(e) {
-
-    // Set game score goal
-    let scoreGoal = 5;
-
-    // Get computer and player selections
-    let computerSelection = computerPlay();
-    let playerSelection = e.currentTarget.getAttribute('data-value');
-
-    // set default round result game winnder
-    let result = 'lose';
-    let gameWinner = 'Player';
-
-    // Prep message variables for later use
-    let roundResultMessage;
-    let gameResultMessage;
-
-
-    if ( playerSelection === 'paper' && computerSelection === 'rock'){
-
-        result = 'win';
-
-    } else if ( playerSelection === 'rock' && computerSelection === 'scissors'){
-
-        result = 'win';
-
-    } else if ( playerSelection === 'scissors' && computerSelection === 'paper' ){
-
-        result = 'win';
-
-    } else if ( playerSelection === computerSelection ){
-
-        result = 'tie';
-
-    }
-
-    // Increase the score variables and update the scoreboard
-    if (result === 'win' ){
-        playerScore++;
-        playerScoreboard.innerText = `Player: ${playerScore}`;
-    } else if( result === 'lose' ) {
-        compScore++;
-        computerScoreboard.innerText = `Computer: ${compScore}`;
-    } 
-
-    // Update the round results message
-    roundResultMessage = `You chose ${playerSelection}, computer chose ${computerSelection}, you ${result}!`;
-    roundResults.innerText = roundResultMessage;
-
-    // update gameWinner if computer reaches scoreGoal
-    if( compScore === scoreGoal ) {
-        gameWinner = 'Computer';
-    }
-
-    // Set and display gameResultMessage
-    gameResultMessage = `The score limit has been reached, ${gameWinner} is the winner`;
-
-    if( compScore >= scoreGoal || playerScore >= scoreGoal ){
-        gameResults.innerText = gameResultMessage;
-
-        resetButton.style.display = "block";
-
-        // Remove event listener as score goal has been reached
-        buttons.forEach((button) => {
-            button.removeEventListener('click', playRound);
-            button.classList.add('disabled');
-        });
-
-    }
 
 }
